5e7dfaa5eaaa34e8114181321591ee22257f8083
arieltaveras <ariel.taveras7278@gmail.com>
2023-11-17 17:00:14 -0500

    google maps implemented

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Box, Container } from '@mui/system';
+import SearchAppBar from './SearchByZip';
 
 export default function FixedContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+    <iframe
+  width="100%"
+  height="450"
+  frameborder="0" style={{border:0}}
+  referrerpolicy="no-referrer-when-downgrade"
+  src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBxMLSZJ_N_UMvfHLWLVdWIr-rHOs-MMmU&q=gas+stations+near+08879"
+  allowfullscreen>
+    </iframe>
       </Container>
     </React.Fragment>
   );
